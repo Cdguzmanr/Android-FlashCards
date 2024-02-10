c287f0a9c98be1c383e86bbcb678584851931097
Carlos Guzmán Ramírez <carlosd.gr.ed@gmail.com>
2024-02-09 18:24:45 -0600

    Flashcards animated

--- app/src/main/java/edu/fvtc/flashcards/MainActivity.java
+++ app/src/main/java/edu/fvtc/flashcards/MainActivity.java
@@ -9,21 +9,33 @@ import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
     public static final String TAG = "MainActivity";
 
     State[] states = {
             new State("Wisconsin", "Milwaukee"),
-            new State("Minesota", "St. Paul"),
+            new State("Minnesota", "St. Paul"),
             new State("Ohio", "Columbus")
     };
 
     int[] imgs = {R.drawable.wisconsin,
-                  R.drawable.minnesota,
-                  R.drawable.ohio
+            R.drawable.minnesota,
+            R.drawable.ohio
+    };
+
+    int[] textfiles = {R.raw.wisconsin,
+            R.raw.minnesota,
+            R.raw.ohio
     };
 
     int cardNo = 0;
@@ -33,12 +45,6 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
 
     GestureDetector gestureDetector;
 
-    // One of those things I have to remember!!!!
-    @Override
-    public boolean onTouchEvent(MotionEvent motionEvent){
-        return gestureDetector.onTouchEvent(motionEvent);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,12 +53,65 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
         imgCard = findViewById(R.id.imageView);
         tvCard = findViewById(R.id.textView);
 
+        updateToNextCard();
+
+        gestureDetector = new GestureDetector(this, this);
+        Log.d(TAG, "onCreate: Complete");
+    }
+
+    private void updateToNextCard()
+    {
+        states[cardNo].setCapital(readFile(textfiles[cardNo]));
+
+        isFront = true;
         imgCard.setVisibility(View.VISIBLE);
         imgCard.setImageResource(imgs[cardNo]);
         tvCard.setText(states[cardNo].getName());
+    }
 
-        gestureDetector = new GestureDetector(this,this);
-        Log.d(TAG, "onCreate: Complete");
+
+    private String readFile(int fileId)
+    {
+        InputStream inputStream;
+        InputStreamReader inputStreamReader;
+        BufferedReader bufferedReader;
+        StringBuffer stringBuffer;
+
+        try{
+            inputStream = getResources().openRawResource(fileId);
+            inputStreamReader = new InputStreamReader(inputStream);
+            bufferedReader = new BufferedReader(inputStreamReader);
+            stringBuffer = new StringBuffer();
+
+            String data;
+
+            while((data = bufferedReader.readLine()) != null)
+            {
+                stringBuffer.append(data).append("\n");
+            }
+
+            // Clean up objects
+            bufferedReader.close();
+            inputStreamReader.close();
+            inputStream.close();
+
+            Log.d(TAG, "readFile: " + stringBuffer.toString());
+            return stringBuffer.toString();
+
+        }
+        catch(Exception e)
+        {
+            Log.d(TAG, "readFile: " + e.getMessage());
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+
+    // One of the those that I have to remember!!!!!!
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent)
+    {
+        return gestureDetector.onTouchEvent(motionEvent);
     }
 
     @Override
@@ -64,55 +123,110 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
     @Override
     public void onShowPress(@NonNull MotionEvent e) {
         Log.d(TAG, "onShowPress: ");
-
     }
 
     @Override
-    public boolean onSingleTapUp(@NonNull MotionEvent e) {
+    public boolean onSingleTapUp(@NonNull MotionEvent motionEvent) {
         Log.d(TAG, "onSingleTapUp: ");
 
         String message;
 
         try {
-            if (isFront) {
-                // go to the back
-                message = "Got to back";
+            if(isFront){
+                // Go to the back
+                message = "Go to back";
                 imgCard.setVisibility(View.INVISIBLE);
                 tvCard.setText(states[cardNo].getCapital());
-            }else {
-                // go to front
-                message = "Got to front";
+            }
+            else{
+                // got to front
+                message = "Go to front";
                 imgCard.setVisibility(View.VISIBLE);
-                imgCard.setImageResource(imgs[cardNo]);
                 tvCard.setText(states[cardNo].getName());
             }
 
-            isFront = !isFront; // better that creating "if (true) change to false"
+            isFront = !isFront;
             Log.d(TAG, "onSingleTapUp: " + message);
-
-        } catch (Exception ex){
-            Log.e(TAG, "onSingleTapUp: " + ex.getMessage() );
-            ex.printStackTrace();
         }
-
-        return false;
+        catch(Exception e)
+        {
+            Log.e(TAG, "onSingleTapUp: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return true;
     }
 
     @Override
     public boolean onScroll(@Nullable MotionEvent e1, @NonNull MotionEvent e2, float distanceX, float distanceY) {
-        Log.d(TAG, "onScroll: ");
+        //Log.d(TAG, "onScroll: ");
         return false;
     }
 
     @Override
     public void onLongPress(@NonNull MotionEvent e) {
         Log.d(TAG, "onLongPress: ");
-
     }
 
     @Override
-    public boolean onFling(@Nullable MotionEvent e1, @NonNull MotionEvent e2, float velocityX, float velocityY) {
+    public boolean onFling(@Nullable MotionEvent motionEvent1,
+                           @NonNull MotionEvent motionEvent2,
+                           float velocityX,
+                           float velocityY) {
         Log.d(TAG, "onFling: ");
-        return false;
+
+        int numCards = states.length;
+
+        try{
+            // Decide which direction I am flinging.
+            int x1 = (int) (motionEvent1 != null ? motionEvent1.getX() : 0);
+            int x2 = (int)motionEvent2.getX();
+
+            if(x1 < x2)
+            {
+                Animation move = AnimationUtils.loadAnimation(this, R.anim.moveright);
+                move.setAnimationListener(new AnimationListener());
+                imgCard.startAnimation(move);
+                tvCard.startAnimation(move);
+                // Swipe right
+                Log.d(TAG, "onFling: Right");
+                cardNo = (cardNo - 1 + numCards) % numCards;
+            }
+            else {
+                Animation move = AnimationUtils.loadAnimation(this, R.anim.moveleft);
+                move.setAnimationListener(new AnimationListener());
+                imgCard.startAnimation(move);
+                tvCard.startAnimation(move);
+                // Swipe left
+                Log.d(TAG, "onFling: Left");
+                cardNo = (cardNo + 1) % numCards;
+            }
+            //updateToNextCard();
+
+        }
+        catch(Exception ex)
+        {
+            Log.e(TAG, "onFling: " + ex.getMessage());
+            ex.printStackTrace();
+        }
+        return true;
+    }
+
+    private class AnimationListener implements Animation.AnimationListener{
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+            Log.d(TAG, "onAnimationEnd: ");
+            updateToNextCard();
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
     }
 }
